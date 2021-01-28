6500bf4be988399495e56a754b010ef15fce9349
Yuhang Zhao <2546789017@qq.com>
2021-01-28 19:53:43 +0800

    FFmpeg: don't use debug config for release builds
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -44,7 +44,7 @@ else()
     set(LIB_PATH_VAR "LIBRARY_PATH")
 endif()
 
-set(OPTIONS "--enable-asm --enable-x86asm --disable-doc --enable-debug --enable-runtime-cpudetect")
+set(OPTIONS "--enable-asm --enable-x86asm --disable-doc --enable-runtime-cpudetect")
 
 if(VCPKG_TARGET_IS_WINDOWS)
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
@@ -380,8 +380,8 @@ if(VCPKG_TARGET_IS_UWP)
     set(OPTIONS_CROSS " --enable-cross-compile --target-os=win32 --arch=${VCPKG_TARGET_ARCHITECTURE}")
 endif()
 
-set(OPTIONS_DEBUG "--debug") # Note: --disable-optimizations can't be used due to http://ffmpeg.org/pipermail/libav-user/2013-March/003945.html
-set(OPTIONS_RELEASE "")
+set(OPTIONS_DEBUG "--enable-debug --debug") # Note: --disable-optimizations can't be used due to http://ffmpeg.org/pipermail/libav-user/2013-March/003945.html
+set(OPTIONS_RELEASE "--disable-debug")
 
 set(OPTIONS "${OPTIONS} ${OPTIONS_CROSS}")
 
@@ -461,7 +461,7 @@ endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
     file(GLOB DEF_FILES ${CURRENT_PACKAGES_DIR}/lib/*.def ${CURRENT_PACKAGES_DIR}/debug/lib/*.def)
-    
+
     if(NOT VCPKG_TARGET_IS_MINGW)
         if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
             set(LIB_MACHINE_ARG /machine:ARM)
@@ -489,7 +489,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
             )
         endforeach()
     endif()
-    
+
     file(GLOB EXP_FILES ${CURRENT_PACKAGES_DIR}/lib/*.exp ${CURRENT_PACKAGES_DIR}/debug/lib/*.exp)
     file(GLOB LIB_FILES ${CURRENT_PACKAGES_DIR}/bin/*${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX} ${CURRENT_PACKAGES_DIR}/debug/bin/*${VCPKG_TARGET_STATIC_LIBRARY_SUFFIX})
     if(VCPKG_TARGET_IS_MINGW)
@@ -522,7 +522,7 @@ vcpkg_copy_pdbs()
 if (VCPKG_TARGET_IS_WINDOWS)
     # Translate cygpath to local path
     set(CYGPATH_CMD "${MSYS_ROOT}/usr/bin/cygpath.exe" -w)
-    
+
     foreach(PKGCONFIG_PATH "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
         file(GLOB PKGCONFIG_FILES "${PKGCONFIG_PATH}/*.pc")
         foreach(PKGCONFIG_FILE IN LISTS PKGCONFIG_FILES)
@@ -551,7 +551,7 @@ if (VCPKG_TARGET_IS_WINDOWS)
             )
             string(REPLACE "\n" "" FIXED_INCLUDE_PATH "${FIXED_INCLUDE_PATH}")
             file(TO_CMAKE_PATH ${FIXED_INCLUDE_PATH} FIXED_INCLUDE_PATH)
-            
+
             vcpkg_replace_string("${PKGCONFIG_FILE}" "${prefix_cygpath}" "${FIXED_PREFIX_PATH}")
             vcpkg_replace_string("${PKGCONFIG_FILE}" "${libdir_cygpath}" "${FIXED_LIBDIR_PATH}")
             vcpkg_replace_string("${PKGCONFIG_FILE}" "${includedir_cygpath}" "${FIXED_INCLUDE_PATH}")
